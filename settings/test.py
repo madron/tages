4971072657efcff25b396d6d6d038840ee2c8d73
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2020-10-04 20:22:09 +0200

    add debug toolbar

--- settings/test.py
+++ settings/test.py
@@ -1,1 +1,10 @@
 from .common import *
+
+
+# Debug Toolbar
+INSTALLED_APPS += [
+    'debug_toolbar',
+    'graphiql_debug_toolbar',
+]
+MIDDLEWARE.insert(1, 'graphiql_debug_toolbar.middleware.DebugToolbarMiddleware')
+INTERNAL_IPS = ['127.0.0.1']
--- settings/urls.py
+++ settings/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.views.decorators.csrf import csrf_exempt
@@ -9,3 +10,10 @@ urlpatterns = [
     path('countries/', include('tages.countries.urls')),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
+
+
+if 'debug_toolbar' in settings.INSTALLED_APPS:
+    import debug_toolbar
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
